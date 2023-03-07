9789d47e1de0c843a939dbf84f8256c3cf2b3272
SY2on <sjk8w7@gmail.com>
2023-03-07 15:39:11 +0900

    kotlin -> java

---
+++ src/test/java/ssu/eatssu/EatSsuApplicationTests.java
@@ -0,0 +1,13 @@
+package ssu.eatssu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatSsuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
