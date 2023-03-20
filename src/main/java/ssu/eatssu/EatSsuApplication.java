776b1e06c5b531e785de2307c602ecff3be91381
SY2on <sjk8w7@gmail.com>
2023-03-20 16:50:31 +0900

    ADD : OpenApi Authorization기능 추가

--- src/main/java/ssu/eatssu/EatSsuApplication.java
+++ src/main/java/ssu/eatssu/EatSsuApplication.java
@@ -1,9 +1,13 @@
 package ssu.eatssu;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://eatssu.shop",description = "Server url"),
+@Server(url = "http://localhost:9000",description = "Local server")})
 @EnableJpaAuditing
 @SpringBootApplication
 public class EatSsuApplication {
