e4d9aa24c863cec79e195e8f8360eb13134dd29f
SY2on <sjk8w7@gmail.com>
2023-03-21 17:22:55 +0900

    ADD : 메뉴, 식당관련 Entity 추가

--- src/main/java/ssu/eatssu/domain/Role.java
+++ src/main/java/ssu/eatssu/domain/Role.java
@@ -1,7 +1,9 @@
 package ssu.eatssu.domain;
 
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 
+@Getter
 public enum Role implements GrantedAuthority {
     USER("ROLE_USER", "유저"),
     ADMIN("ROLE_ADMIN", "관리자");
@@ -19,7 +21,4 @@ public enum Role implements GrantedAuthority {
         return authority;
     }
 
-    public String getDescription() {
-        return description;
-    }
 }
--- src/main/java/ssu/eatssu/domain/TimePart.java
+++ src/main/java/ssu/eatssu/domain/TimePart.java
@@ -1,14 +1,24 @@
 package ssu.eatssu.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
 
 import java.util.Locale;
 
+@Getter
 public enum TimePart {
-    MORNING, LUNCH, DINNER;
+    MORNING("조식"),
+    LUNCH("중식"),
+    DINNER("석식");
 
     @JsonCreator
     public static TimePart from(String s){
         return TimePart.valueOf(s.toUpperCase(Locale.ROOT));
+    }
+
+    private String name;
+
+    TimePart(String name){
+        this.name = name;
     }
 }
