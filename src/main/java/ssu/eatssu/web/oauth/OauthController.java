331b68cc2292d1ae9bc0948efba7b898472932bb
SY2on <sjk8w7@gmail.com>
2023-08-08 04:13:19 +0900

    Feat: 애플 로그인 API 개발, 리뷰신고 entity 추가

--- src/main/java/ssu/eatssu/web/oauth/OauthController.java
+++ src/main/java/ssu/eatssu/web/oauth/OauthController.java
@@ -6,15 +6,23 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ssu.eatssu.domain.repository.UserRepository;
+import ssu.eatssu.response.BaseException;
+import ssu.eatssu.response.BaseResponse;
 import ssu.eatssu.service.OauthService;
 import ssu.eatssu.utils.SecurityUtil;
+import ssu.eatssu.web.oauth.dto.AppleLogin;
 import ssu.eatssu.web.oauth.dto.KakaoLogin;
 import ssu.eatssu.web.user.dto.*;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+@Slf4j
 @RestController
 @RequestMapping("/oauth")
 @RequiredArgsConstructor
@@ -33,6 +41,22 @@ public class OauthController {
     public ResponseEntity<Tokens> kakaoLogin(@Valid @RequestBody KakaoLogin login) throws JsonProcessingException {
         Tokens tokens = oauthService.loginByKakao(login.getEmail(), login.getProviderId());
         return ResponseEntity.ok(tokens);
+    }
+
+    /**
+     * 애플 로그인, 회원가입
+     */
+    @Operation(summary = "애플 로그인, 회원가입", description = "애플 로그인, 회원가입")
+    @PostMapping("/apple")
+    public ResponseEntity<Tokens> appleLogin(@Valid @RequestBody AppleLogin login) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        Tokens tokens = oauthService.loginByApple(login.getIdentityToken());
+        return ResponseEntity.ok(tokens);
+    }
+
+    @ExceptionHandler(BaseException.class)
+    public BaseResponse<String> handleBaseException(BaseException e) {
+        log.info(e.getStatus().toString());
+        return new BaseResponse<>(e.getStatus());
     }
 
 
