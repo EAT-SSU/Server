b9a5b54b09dae3d9e536f813957e0e6e4840e863
SY2on <sjk8w7@gmail.com>
2023-08-11 23:09:43 +0900

    Feat: 리뷰 신고 기능 개발

---
+++ src/main/java/ssu/eatssu/domain/repository/ReviewReportRepository.java
@@ -0,0 +1,7 @@
+package ssu.eatssu.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ssu.eatssu.domain.ReviewReport;
+
+public interface ReviewReportRepository extends JpaRepository<ReviewReport, Long>{
+}
---
+++ src/main/java/ssu/eatssu/web/report/ReportController.java
@@ -0,0 +1,50 @@
+package ssu.eatssu.web.report;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ssu.eatssu.response.BaseException;
+import ssu.eatssu.response.BaseResponse;
+import ssu.eatssu.service.MyPageService;
+import ssu.eatssu.service.ReportService;
+import ssu.eatssu.utils.SecurityUtil;
+import ssu.eatssu.web.SliceDto;
+import ssu.eatssu.web.mypage.dto.MyReviewDetail;
+import ssu.eatssu.web.mypage.dto.MypageInfo;
+import ssu.eatssu.web.report.dto.ReviewReportCreate;
+
+@Slf4j
+@RestController
+@RequestMapping("/report")
+@RequiredArgsConstructor
+@Tag(name = "Report", description = "신고 API")
+public class ReportController {
+    private final ReportService reportService;
+
+    /**
+     * 리뷰 신고하기
+     */
+    @Operation(summary = "리뷰 신고하기", description = "리뷰 신고하기")
+    @PostMapping("/")
+    public ResponseEntity<String> reportReview(@RequestBody ReviewReportCreate reviewReportCreate) {
+        Long userId = SecurityUtil.getLoginUserId();
+        reportService.reportReview(userId, reviewReportCreate);
+        return ResponseEntity.ok("Success");
+    }
+
+    @ExceptionHandler(BaseException.class)
+    public BaseResponse<String> handleBaseException(BaseException e) {
+        log.info(e.getStatus().toString());
+        return new BaseResponse<>(e.getStatus());
+    }
+
+}
