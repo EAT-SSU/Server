1dba02cce96d0ed3a00763cb27667b6bb5de37ab
SY2on <sjk8w7@gmail.com>
2023-12-02 16:21:18 +0900

    Chore: Application Setting

--- src/main/java/ssu/eatssu/EatSsuApplication.java
+++ src/main/java/ssu/eatssu/EatSsuApplication.java
@@ -1,2 +1,15 @@
-package ssu.eatssu;public class EatSsuApplication {
+package ssu.eatssu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class EatSsuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatSsuApplication.class, args);
+    }
+
 }
