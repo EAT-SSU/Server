531d560914181e348b8f33102f85333da1335662
SY2on <sjk8w7@gmail.com>
2023-12-03 14:42:18 +0900

    Feat: 문의 남기기 API 개발 문의 남기는 API 개발 input data - 문의내용 text만 입력 받음 #45

---
+++ src/main/java/ssu/eatssu/service/UserInquiriesService.java
@@ -0,0 +1,28 @@
+package ssu.eatssu.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ssu.eatssu.domain.User;
+import ssu.eatssu.domain.UserInquiries;
+import ssu.eatssu.domain.repository.UserInquiriesRepository;
+import ssu.eatssu.domain.repository.UserRepository;
+import ssu.eatssu.response.BaseException;
+
+
+import static ssu.eatssu.response.BaseResponseStatus.*;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class UserInquiriesService {
+
+    private final UserRepository userRepository;
+    private final UserInquiriesRepository userInquiriesRepository;
+
+    public void createUserInquiries(Long userId, String content) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new BaseException(NOT_FOUND_USER));
+        UserInquiries userInquiries = UserInquiries.builder().user(user).content(content).build();
+        userInquiriesRepository.save(userInquiries);
+    }
+}
---
+++ src/main/java/ssu/eatssu/web/user_inquiries/UserInquiriesController.java
@@ -0,0 +1,43 @@
+package ssu.eatssu.web.user_inquiries;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ssu.eatssu.domain.repository.UserRepository;
+import ssu.eatssu.response.BaseException;
+import ssu.eatssu.response.BaseResponse;
+import ssu.eatssu.service.UserInquiriesService;
+import ssu.eatssu.slack.SlackService;
+import ssu.eatssu.utils.SecurityUtil;
+import ssu.eatssu.web.user_inquiries.dto.UserInquiriesCreate;
+
+@Slf4j
+@RestController
+@RequestMapping("/inquiries")
+@RequiredArgsConstructor
+@Tag(name = "Inquiries", description = "문의 API")
+public class UserInquiriesController {
+    private final SlackService slackService;
+    private final UserRepository userRepository;
+    private final UserInquiriesService userInquiriesService;
+
+    /**
+     * 문의 남기기
+     */
+    @Operation(summary = "문의 남기기", description = "문의 남기기")
+    @PostMapping("/")
+    public BaseResponse<String> userInquiriesCreate(@RequestBody UserInquiriesCreate userInquiriesCreate) {
+        Long userId = SecurityUtil.getLoginUserId();
+        userInquiriesService.createUserInquiries(userId, userInquiriesCreate.getContent());
+        return new BaseResponse<>("");
+    }
+
+    @ExceptionHandler(BaseException.class)
+    public BaseResponse<String> handleBaseException(BaseException e) {
+        log.info(e.getStatus().toString());
+        return new BaseResponse<>(e.getStatus());
+    }
+
+}
