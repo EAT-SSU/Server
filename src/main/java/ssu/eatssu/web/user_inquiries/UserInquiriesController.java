34212b312579b3885b54a5c6ca07f1ec5b2b6418
SY2on <sjk8w7@gmail.com>
2023-12-04 02:51:25 +0900

    Feat: 고객 문의내역 조회 By Id API 개발 UserInquiriesId로 문의내역 세부사항 조회 가능 로그인 필요 X -> 후에 ADMIN으로 빼야 함 #45

--- src/main/java/ssu/eatssu/web/user_inquiries/UserInquiriesController.java
+++ src/main/java/ssu/eatssu/web/user_inquiries/UserInquiriesController.java
@@ -1,20 +1,26 @@
 package ssu.eatssu.web.user_inquiries;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ssu.eatssu.domain.UserInquiries;
+import ssu.eatssu.domain.repository.UserInquiriesRepository;
 import ssu.eatssu.domain.repository.UserRepository;
 import ssu.eatssu.response.BaseException;
 import ssu.eatssu.response.BaseResponse;
+import ssu.eatssu.response.BaseResponseStatus;
 import ssu.eatssu.service.UserInquiriesService;
 import ssu.eatssu.slack.SlackChannel;
 import ssu.eatssu.slack.SlackMessageFormat;
 import ssu.eatssu.slack.SlackService;
 import ssu.eatssu.utils.SecurityUtil;
 import ssu.eatssu.web.user_inquiries.dto.UserInquiriesCreate;
+import ssu.eatssu.web.user_inquiries.dto.UserInquiriesDetail;
+
+import static ssu.eatssu.response.BaseResponseStatus.*;
 
 @Slf4j
 @RestController
@@ -24,6 +30,7 @@ import ssu.eatssu.web.user_inquiries.dto.UserInquiriesCreate;
 public class UserInquiriesController {
     private final SlackService slackService;
     private final UserRepository userRepository;
+    private final UserInquiriesRepository userInquiriesRepository;
     private final UserInquiriesService userInquiriesService;
 
     /**
@@ -37,6 +44,20 @@ public class UserInquiriesController {
         slackService.sendSlackMessage(SlackMessageFormat.sendUserInquiries(inquiries)
                 , SlackChannel.USER_INQUIRIES_CHANNEL);
         return new BaseResponse<>("");
+    }
+
+    /**
+     * 문의내역 조회 By Id
+     */
+    @Operation(summary = "고객 문의 내역 조회", description = "고객 문의 내역 조회 By Id")
+    @GetMapping("/{userInquiriesId}")
+    public BaseResponse<UserInquiriesDetail> userInquiriesCreate(@Parameter(description = "userInquiriesId")
+                                                        @PathVariable("userInquiriesId") Long userInquiriesId) {
+        UserInquiries inquiries =
+                userInquiriesRepository.findById(userInquiriesId).orElseThrow(()-> new BaseException(NOT_FOUND_USER_INQUIRIES));
+        slackService.sendSlackMessage(SlackMessageFormat.sendUserInquiries(inquiries)
+                , SlackChannel.USER_INQUIRIES_CHANNEL);
+        return new BaseResponse<>(UserInquiriesDetail.fromUserInquiries(inquiries));
     }
 
     @ExceptionHandler(BaseException.class)
