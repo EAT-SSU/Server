72ea6f3c666a84736f3c10239bd0e84af4d2364a
SY2on <sjk8w7@gmail.com>
2023-12-30 22:08:34 +0900

    Comment: Controller 주석 수정 및 추가

--- src/main/java/ssu/eatssu/web/user/UserController.java
+++ src/main/java/ssu/eatssu/web/user/UserController.java
@@ -7,8 +7,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ssu.eatssu.domain.repository.UserRepository;
 import ssu.eatssu.response.BaseException;
@@ -33,6 +31,9 @@ public class UserController {
 
     /**
      * 회원가입
+     * <p>자체 회원가입</p>
+     *
+     * @deprecated 현재는 카카오, 애플 로그인만 사용 중입니다.
      */
     @Operation(summary = "회원가입", description = "회원가입.")
     @PostMapping("/join")
@@ -42,7 +43,8 @@ public class UserController {
     }
 
     /**
-     * 이메일 중복체크. 중복이면 true
+     * 이메일 중복 검사
+     * <p>중복되지 않은 이메일인 경우 true를 반환합니다.</p>
      */
     @Operation(summary = "이메일 중복 체크", description = "통과하면 true, 존재하는 이메일이면 errorCode 2011")
     @PostMapping("/user-emails/{email}/exist")
@@ -57,7 +59,8 @@ public class UserController {
     }
 
     /**
-     * 로그인
+     * 공통 로그인
+     * <p><s>자체, </s>카카오, 애플 로그인 공용으로 사용합니다.</p>
      */
     @Operation(summary = "로그인", description = "로그인")
     @PostMapping("/login")
@@ -68,6 +71,7 @@ public class UserController {
 
     /**
      * 닉네임 수정
+     * <p>닉네임을 수정합니다.</p>
      */
     @Operation(summary = "닉네임 수정", description = "닉네임 수정")
     @PatchMapping("/nickname")
@@ -78,7 +82,8 @@ public class UserController {
     }
 
     /**
-     * 닉네임 중복 체크
+     * 닉네임 중복 검사
+     * <p>중복되지 않은 닉네임인 경우 true를 반환합니다.</p>
      */
     @Operation(summary = "닉네임 중복 체크", description = "통과하면 true, 존재하는 닉네임이면 errorCode 2012")
     @GetMapping("/check-nickname")
@@ -95,6 +100,7 @@ public class UserController {
 
     /**
      * 비밀번호 변경
+     * <p>비밀번호를 변경합니다.</p>
      */
     @Operation(summary = "비밀번호 변경", description = "비밀번호 변경")
     @PatchMapping("/password")
@@ -105,7 +111,8 @@ public class UserController {
     }
 
     /**
-     * accessToken, refreshToken 재발급
+     * AccessToken, RefreshToken 재발급
+     * <p>유효한 Token을 받으면 AccessToken, RefreshToken을 새로 발급합니다.</p>
      */
     @Operation(summary = "accessToken, refreshToken 재발급", description = "accessToken, refreshToken 재발급")
     @PostMapping("/token/reissue")
@@ -115,6 +122,7 @@ public class UserController {
     }
     /**
      * 유저 탈퇴
+     * <p>유저를 탈퇴처리합니다.</p>
      */
     @Operation(summary = "유저 탈퇴", description = "탈퇴 성공하면 true 반환")
     @DeleteMapping("/signout")
