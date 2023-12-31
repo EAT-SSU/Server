37996ad07e33b56d9047eaf6e82fc8962706f3f6
SY2on <sjk8w7@gmail.com>
2024-01-01 02:42:27 +0900

    Rename: Inquiries -> Inquiry

--- src/main/java/ssu/eatssu/slack/SlackChannel.java
+++ src/main/java/ssu/eatssu/slack/SlackChannel.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 public enum SlackChannel {
 
-    REPORT_CHANNEL("#신고"), ADDMENU_CHANNEL("#메뉴_추가"), ERROR_CHANNEL("#장애"), USER_INQUIRIES_CHANNEL("#유저-문의");
+    REPORT_CHANNEL("#신고"), ADDMENU_CHANNEL("#메뉴_추가"), ERROR_CHANNEL("#장애"), USER_INQUIRY_CHANNEL("#유저-문의");
 
     @Getter
     private String krName;
