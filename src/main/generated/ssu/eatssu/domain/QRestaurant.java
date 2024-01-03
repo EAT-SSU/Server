ac872139db016e6e5fe62cf97396fbc0ec14ed12
SY2on <sjk8w7@gmail.com>
2024-01-04 02:35:07 +0900

    Remove: OpenHour 삭제 이제 안씀

--- src/main/generated/ssu/eatssu/domain/QRestaurant.java
+++ src/main/generated/ssu/eatssu/domain/QRestaurant.java
@@ -22,13 +22,9 @@ public class QRestaurant extends EntityPathBase<Restaurant> {
 
     public final NumberPath<Long> id = createNumber("id", Long.class);
 
-    public final StringPath location = createString("location");
-
     public final ListPath<Meal, QMeal> meals = this.<Meal, QMeal>createList("meals", Meal.class, QMeal.class, PathInits.DIRECT2);
 
     public final ListPath<Menu, QMenu> menus = this.<Menu, QMenu>createList("menus", Menu.class, QMenu.class, PathInits.DIRECT2);
-
-    public final ListPath<OpenHour, QOpenHour> openHours = this.<OpenHour, QOpenHour>createList("openHours", OpenHour.class, QOpenHour.class, PathInits.DIRECT2);
 
     public final EnumPath<ssu.eatssu.domain.enums.RestaurantName> restaurantName = createEnum("restaurantName", ssu.eatssu.domain.enums.RestaurantName.class);
 
--- src/main/java/ssu/eatssu/domain/Restaurant.java
+++ src/main/java/ssu/eatssu/domain/Restaurant.java
@@ -20,14 +20,9 @@ public class Restaurant {
     @Enumerated(EnumType.STRING)
     private RestaurantName restaurantName;
 
-    private String location;
-
     @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
     private List<Menu> menus;
 
     @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
     private List<Meal> meals;
-
-    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
-    private List<OpenHour> openHours;
 }
