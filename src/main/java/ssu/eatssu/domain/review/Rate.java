4613eccede08ced4ef6eacaf6500c1b37987f17d
Geonwoo Pack <packdev937@gmail.com>
2024-01-07 12:34:33 +0900

    feat : 평점을 관리하는 VO 객체를 생성한다

---
+++ src/main/java/ssu/eatssu/domain/review/Rate.java
@@ -0,0 +1,31 @@
+package ssu.eatssu.domain.review;
+
+import io.jsonwebtoken.lang.Assert;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+@Embeddable
+public class Rate {
+
+    private Integer mainRate;
+
+    private Integer amountRate;
+
+    private Integer tasteRate;
+
+    private Rate(Integer mainRate, Integer amountRate, Integer tasteRate) {
+        Assert.isTrue(mainRate >= 0 && mainRate <= 5, "mainRate must be between 0 and 5");
+        Assert.isTrue(amountRate >= 0 && amountRate <= 5, "amountRate must be between 0 and 5");
+        Assert.isTrue(tasteRate >= 0 && tasteRate <= 5, "tasteRate must be between 0 and 5");
+        this.mainRate = mainRate;
+        this.amountRate = amountRate;
+        this.tasteRate = tasteRate;
+    }
+
+    public static Rate of(Integer mainRate, Integer amountRate, Integer tasteRate) {
+        return new Rate(mainRate, amountRate, tasteRate);
+    }
+}
