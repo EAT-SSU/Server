df32c6adca56383243990a7227fb37e69847ff59
Geonwoo Pack <packdev937@gmail.com>
2024-01-07 12:10:33 +0900

    refactor : DateUtils로 관련 메소드를 리팩토링 한다

---
+++ src/main/java/ssu/eatssu/utils/DateUtils.java
@@ -0,0 +1,25 @@
+package ssu.eatssu.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import ssu.eatssu.handler.response.BaseException;
+import ssu.eatssu.handler.response.BaseResponseStatus;
+
+public class DateUtils {
+
+    public static Date toDate(String date) throws BaseException {
+
+        try {
+            return DateUtils.toDate("yyyyMMdd", date);
+        } catch (ParseException e) {
+            throw new BaseException(BaseResponseStatus.INVALID_DATE);
+        }
+
+    }
+
+    public static Date toDate(String dateFormat, String dateString) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+        return simpleDateFormat.parse(dateString);
+    }
+}
