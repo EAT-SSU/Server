67fadb6626faeafc9fb6f7ae4669cf72b214c73f
SY2on <sjk8w7@gmail.com>
2024-01-13 10:14:11 +0900

    Refactor: JPA Projection 을 이용하여 RatingCalculator 구현 JPA 객체 참조를 이용한 기존의 RatingCalculator(JpaLoadCollectionRatingCalculator) 과의 성능 비교 필요

---
+++ src/main/java/ssu/eatssu/domain/rating/JpaProjectionRatingCalculator.java
@@ -0,0 +1,125 @@
+package ssu.eatssu.domain.rating;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ssu.eatssu.domain.menu.Meal;
+import ssu.eatssu.domain.menu.Menu;
+import ssu.eatssu.domain.repository.ReviewRepository;
+import ssu.eatssu.domain.repository.dto.RatingsDto;
+import ssu.eatssu.web.review.dto.RatingAverages;
+import ssu.eatssu.web.review.dto.ReviewRatingCount;
+
+import java.util.Collection;
+
+/**
+ * Jpa projection 을 이용하여 구현한 클래스입니다.
+ * 더미 데이터
+ * JpaLoadCollectionRatingCalculator : 253 lines
+ * JpaProjectionRatingCalculator : 174 lines
+ */
+
+@Component
+@RequiredArgsConstructor
+public class JpaProjectionRatingCalculator implements RatingCalculator {
+
+    private final ReviewRepository reviewRepository;
+
+    @Override
+    public ReviewRatingCount mealRatingCount(Meal meal) {
+        ReviewRating.resetAll();
+
+        Collection<RatingsDto> mealRatings = reviewRepository.findByMenu_MealMenus_Meal(meal, RatingsDto.class);
+        mealRatings.forEach(ratings -> ReviewRating.fromValue(ratings.getMainRating()).incrementCount());
+
+        return ReviewRating.toResponse();
+    }
+
+    @Override
+    public ReviewRatingCount menuRatingCount(Menu menu) {
+        return null;
+    }
+
+    @Override
+    public RatingAverages mealAverageRatings(Meal meal) {
+        int totalReviewCount = mealTotalReviewCount(meal);
+
+        if (totalReviewCount == 0)
+            return new RatingAverages(null, null, null);
+
+        Collection<RatingsDto> mealRatings = reviewRepository.findByMenu_MealMenus_Meal(meal, RatingsDto.class);
+
+        Integer totalMainRating = mealRatings.stream().mapToInt(RatingsDto::getMainRating).sum();
+        Integer totalTasteRating = mealRatings.stream().mapToInt(RatingsDto::getTasteRating).sum();
+        Integer totalAmountRating = mealRatings.stream().mapToInt(RatingsDto::getAmountRating).sum();
+
+        return RatingAverages.builder()
+                .mainRating(averageRating(totalMainRating, totalReviewCount))
+                .tasteRating(averageRating(totalTasteRating, totalReviewCount))
+                .amountRating(averageRating(totalAmountRating, totalReviewCount))
+                .build();
+    }
+
+    @Override
+    public RatingAverages menuAverageRatings(Menu menu) {
+        return null;
+    }
+
+    @Override
+    public Double mealAverageMainRating(Meal meal) {
+        int totalReviewCount = mealTotalReviewCount(meal);
+
+        if (totalReviewCount == 0) {
+            return null;
+        }
+
+        return averageRating(mealTotalMainRating(meal), totalReviewCount);
+    }
+
+    @Override
+    public Double menuAverageMainRating(Menu menu) {
+        return null;
+    }
+
+    @Override
+    public int mealTotalReviewCount(Meal meal) {
+        return reviewRepository.countByMenu_MealMenus_Meal(meal).intValue();
+    }
+
+    @Override
+    public Double averageRating(Integer totalRating, int totalReviewCount) {
+        if (totalRating == null || totalReviewCount == 0) {
+            return null;
+        }
+        return totalRating / (double) totalReviewCount;
+    }
+
+    @Override
+    public Integer mealTotalMainRating(Meal meal) {
+        return null;
+    }
+
+    @Override
+    public Integer mealTotalAmountRating(Meal meal) {
+        return null;
+    }
+
+    @Override
+    public Integer mealTotalTasteRating(Meal meal) {
+        return null;
+    }
+
+    @Override
+    public Integer menuTotalMainRating(Menu menu) {
+        return null;
+    }
+
+    @Override
+    public Integer menuTotalAmountRating(Menu menu) {
+        return null;
+    }
+
+    @Override
+    public Integer menuTotalTasteRating(Menu menu) {
+        return null;
+    }
+}
--- src/main/java/ssu/eatssu/domain/repository/ReviewRepository.java
+++ src/main/java/ssu/eatssu/domain/repository/ReviewRepository.java
@@ -1,11 +1,21 @@
 package ssu.eatssu.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ssu.eatssu.domain.menu.Meal;
 import ssu.eatssu.domain.menu.Menu;
 import ssu.eatssu.domain.review.Review;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
     List<Review> findAllByMenu(Menu menu);
+
+    /**
+     * dto projection
+     */
+    <T> Collection<T>  findByMenu_MealMenus_Meal(Meal meal, Class<T> type);
+
+    Long countByMenu_MealMenus_Meal(Meal meal);
+
 }
