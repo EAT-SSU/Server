37dbfbffb75525056b0d32592847870b9797c9cd
Geonwoo Pack <packdev937@gmail.com>
2024-01-15 15:19:05 +0900

    [#83] Feat : MenuController와 MealController로 분리한다

---
+++ src/main/java/ssu/eatssu/domain/menu/presentation/MenuController.java
@@ -0,0 +1,62 @@
+package ssu.eatssu.domain.menu.presentation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ssu.eatssu.domain.menu.dto.MenuResponse.FixMenuInfo;
+import ssu.eatssu.domain.menu.dto.MenuResponse.MenuList;
+import ssu.eatssu.domain.restaurant.RestaurantName;
+import ssu.eatssu.global.handler.response.BaseException;
+import ssu.eatssu.handler.response.BaseResponse;
+import ssu.eatssu.domain.menu.service.MenuService;
+
+import java.util.List;
+
+import static ssu.eatssu.handler.response.BaseResponseStatus.NOT_SUPPORT_RESTAURANT;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/menus")
+@Tag(name = "Menu", description = "메뉴 API")
+public class MenuController {
+
+    private final MenuService menuService;
+
+    @Operation(summary = "고정 메뉴 리스트 조회", description = """
+        고정 메뉴 리스트를 조회하는 API 입니다.
+        메뉴가 고정된 식당(푸드코트, 스낵코너, <s>더 키친</s>)의 메뉴 리스트를 조회합니다.
+        """)
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "메뉴 리스트 조회 성공", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+        @ApiResponse(responseCode = "400", description = "지원하지 않는 식당(변동 메뉴 식당)", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+        @ApiResponse(responseCode = "404", description = "존재하지 않는 식당", content = @Content(schema = @Schema(implementation = BaseResponse.class)))
+    })
+    @GetMapping("/list")
+    public BaseResponse<List<FixMenuInfo>> getMenus(
+        @RequestParam("restaurant") RestaurantName restaurantName) {
+        if (RestaurantName.isVariable(restaurantName)) {
+            throw new BaseException(NOT_SUPPORT_RESTAURANT);
+        }
+        return BaseResponse.success(menuService.findMenus(restaurantName));
+    }
+
+    @Operation(summary = "메뉴 정보 리스트 조회", description = """
+        메뉴 정보 리스트를 조회하는 API 입니다.<br><br>
+        식단식별자(mealId)로 해당 식단에 속하는 메뉴 정보 목록을 조회합니다.")
+        """)
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "메뉴 정보 리스트 조회 성공", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+        @ApiResponse(responseCode = "404", description = "존재하지 않는 식단", content = @Content(schema = @Schema(implementation = BaseResponse.class)))
+    })
+    @GetMapping("/in-meal")
+    public BaseResponse<MenuList> getMenusInMeal(@Parameter(description = "mealId")
+    @RequestParam("mealId") Long mealId) {
+        return BaseResponse.success(menuService.findMenuListInMeal(mealId));
+    }
+}
