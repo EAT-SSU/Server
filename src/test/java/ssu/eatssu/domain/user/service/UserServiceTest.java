e022c91bff9c1dd68db5ea6d1ee9e5dd9632e21b
Geonwoo Pack <packdev937@gmail.com>
2024-01-18 18:37:05 +0900

    [#83] test : 회원 탈퇴와 닉네임 변경을 테스트 한다

--- src/test/java/ssu/eatssu/domain/user/service/UserServiceTest.java
+++ src/test/java/ssu/eatssu/domain/user/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package ssu.eatssu.domain.user.service;
 import static org.assertj.core.api.Assertions.*;
 
 import org.assertj.core.api.Assertions;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,10 +31,45 @@ class UserServiceTest {
     @Test
     void 회원가입을_한다() {
         // given & when
-        userService.join("test@test.com", OAuthProvider.EATSSU, "1234");
+        회원가입_요청();
 
         // then
         assertThat(userRepository.findAll()).hasSize(1);
     }
 
+    @Test
+    void 닉네임을_변경한다() {
+        // given
+        User user = 회원가입_요청();
+        CustomUserDetails userDetails = UserDetails_생성(user);
+
+        // when
+        userService.updateNickname(userDetails, new NicknameUpdateRequest("newNickname"));
+
+        // then
+        user = userRepository.findById(userDetails.getId()).orElseThrow();
+        assertThat(user.getNickname()).isEqualTo("newNickname");
+    }
+
+    @Test
+    void 회원탈퇴를_한다() {
+        // given
+        User user = 회원가입_요청();
+        CustomUserDetails userDetails = UserDetails_생성(user);
+
+        // when
+        userService.withdraw(userDetails);
+
+        // then
+        assertThat(userRepository.findAll()).hasSize(0);
+    }
+
+    private User 회원가입_요청() {
+        return userService.join("test@test.com", OAuthProvider.EATSSU, "1234");
+    }
+
+    @NotNull
+    private CustomUserDetails UserDetails_생성(User user) {
+        return new CustomUserDetails(user);
+    }
 }
