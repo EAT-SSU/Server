bf959b9934730e5c487c922e0fa2907c23f556c5
SY2on <sjk8w7@gmail.com>
2024-01-20 20:41:33 +0900

    [#42] Feat: 고정 메뉴 조회 Dto 변경 및 추가

--- src/main/java/ssu/eatssu/domain/admin/dto/FixMenuBoard.java
+++ src/main/java/ssu/eatssu/domain/admin/dto/FixMenuBoard.java
@@ -1,9 +1,14 @@
 package ssu.eatssu.domain.admin.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public record FixMenuBoard(String restaurantName, List<BriefMenu> menus ) implements RestaurantMenuBoard {
-    public void addMenu(BriefMenu menu) {
+public record FixMenuBoard(String restaurantName, List<MenuLine> menus) implements RestaurantMenuBoard {
+    public FixMenuBoard(String restaurantName) {
+        this(restaurantName, new ArrayList<>());
+    }
+
+    public void addMenu(MenuLine menu) {
         menus.add(menu);
     }
 }
