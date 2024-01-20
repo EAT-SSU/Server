cd63d46d8943b82e06577e29e11b2d73f975c976
SY2on <sjk8w7@gmail.com>
2024-01-20 20:41:33 +0900

    [#42] Feat: 고정 메뉴 조회 Dto 변경 및 추가

--- src/main/java/ssu/eatssu/domain/admin/dto/BriefMenu.java
+++ src/main/java/ssu/eatssu/domain/admin/dto/BriefMenu.java
@@ -1,4 +1,4 @@
 package ssu.eatssu.domain.admin.dto;
 
-public record BriefMenu(Long id, String menuName, Integer price, Double rating) {
+public record BriefMenu(Long id, String name, Integer price) {
 }
