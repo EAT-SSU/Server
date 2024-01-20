1460ce27b35d2af26fed906087f348e461b12ba0
SY2on <sjk8w7@gmail.com>
2024-01-20 19:50:42 +0900

    [#42] Refactor: RestaurantName 사용하지 않는 메소드 삭제

--- src/main/java/ssu/eatssu/domain/restaurant/entity/RestaurantName.java
+++ src/main/java/ssu/eatssu/domain/restaurant/entity/RestaurantName.java
@@ -1,41 +1,33 @@
 package ssu.eatssu.domain.restaurant.entity;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import java.util.Arrays;
 import lombok.Getter;
-
 import ssu.eatssu.global.handler.response.BaseException;
 import ssu.eatssu.global.handler.response.BaseResponseStatus;
+
+import java.util.Arrays;
 
 @Getter
 public enum RestaurantName {
     DODAM("도담 식당", 6000),
-    DORMITORY( "기숙사 식당", 5000),
-    FOOD_COURT( "푸드 코트", null),
-    SNACK_CORNER( "스낵 코너", null),
-    HAKSIK( "학생 식당", 5000);
+    DORMITORY("기숙사 식당", 5000),
+    FOOD_COURT("푸드 코트", null),
+    SNACK_CORNER("스낵 코너", null),
+    HAKSIK("학생 식당", 5000);
 
     private String description;
     private Integer price;
-
-    @JsonCreator
-    public static RestaurantName from(String description) {
-        return Arrays.stream(RestaurantName.values())
-            .filter(d -> d.getDescription().equals(description))
-            .findAny()
-            .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_RESTAURANT));
-    }
 
     RestaurantName(String description, Integer price) {
         this.description = description;
         this.price = price;
     }
 
-    public static boolean isFixed(RestaurantName restaurantName) {
-        return restaurantName.getRestaurantType() == RestaurantType.FIXED;
-    }
-
-    public static boolean isVariable(RestaurantName restaurantName) {
-        return restaurantName.getRestaurantType() == RestaurantType.VARIABLE;
+    @JsonCreator
+    public static RestaurantName from(String description) {
+        return Arrays.stream(RestaurantName.values())
+                .filter(d -> d.getDescription().equals(description))
+                .findAny()
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_RESTAURANT));
     }
 }
