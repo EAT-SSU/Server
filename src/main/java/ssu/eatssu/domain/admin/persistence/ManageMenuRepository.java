9c66422e0351afd553cbf9b8865f11f9221309cc
SY2on <sjk8w7@gmail.com>
2024-01-21 00:06:44 +0900

    [#42] Refactor: ManageMenuRepository EntityManager 직접 이용 -> JpaRepository 상속으로 변경

--- src/main/java/ssu/eatssu/domain/admin/persistence/ManageMenuRepository.java
+++ src/main/java/ssu/eatssu/domain/admin/persistence/ManageMenuRepository.java
@@ -1,51 +1,12 @@
 package ssu.eatssu.domain.admin.persistence;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.data.jpa.repository.support.JpaEntityInformation;
-import org.springframework.data.jpa.repository.support.JpaEntityInformationSupport;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ssu.eatssu.domain.menu.entity.Menu;
 
+import java.util.Optional;
+
 @Repository
-public class ManageMenuRepository {
-
-    @PersistenceContext
-    private final EntityManager em;
-
-    private final JpaEntityInformation<Menu, ?> entityInformation;
-
-    public ManageMenuRepository(EntityManager em) {
-        this.em = em;
-        this.entityInformation =
-                JpaEntityInformationSupport.getEntityInformation(Menu.class, em);
-    }
-
-    @Transactional
-    public Menu save(Menu menu) {
-        if (entityInformation.isNew(menu)) {
-            em.persist(menu);
-            return menu;
-        } else {
-            return em.merge(menu);
-        }
-    }
-
-    @Transactional
-    public void delete(Menu menu) {
-
-        if (entityInformation.isNew(menu)) {
-            return;
-        }
-
-        Menu existing = em.find(Menu.class, menu.getId());
-
-        if (existing == null) {
-            return;
-        }
-
-        em.remove(em.contains(menu) ? menu : em.merge(menu));
-    }
-
+public interface ManageMenuRepository extends JpaRepository<Menu, Long> {
+    Optional<Menu> findById(Long id);
 }
