7b13fb51f3c5361b1e9323ba4a84074d25c2567a
SY2on <sjk8w7@gmail.com>
2024-01-20 16:25:07 +0900

    [#42] Feat: AdminAuth 값 properties 등록

---
+++ src/main/java/ssu/eatssu/domain/admin/infrastructure/AdminConfiguration.java
@@ -0,0 +1,16 @@
+package ssu.eatssu.domain.admin.infrastructure;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ssu.eatssu.domain.admin.controller.AdminAuth;
+
+@Configuration
+@EnableConfigurationProperties(AdminAuthConfigurationProperties.class)
+public class AdminConfiguration {
+
+    @Bean
+    public AdminAuth adminAuthConfigurationProperties(AdminAuthConfigurationProperties properties) {
+        return new AdminAuth(properties.getLoginId());
+    }
+}
