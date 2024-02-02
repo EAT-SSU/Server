c21079e75672c34e0e60e42a4f2957ac5f31dcae
SY2on <sjk8w7@gmail.com>
2024-02-02 15:18:14 +0900

    [#78] Refactor: Restaurant 에 type 구분 필드 제거

--- src/main/java/ssu/eatssu/domain/restaurant/entity/Restaurant.java
+++ src/main/java/ssu/eatssu/domain/restaurant/entity/Restaurant.java
@@ -10,15 +10,15 @@ import ssu.eatssu.global.handler.response.BaseResponseStatus;
 
 @Getter
 public enum Restaurant {
-    DODAM(RestaurantType.VARIABLE, "도담 식당", 6000),
-    DORMITORY(RestaurantType.VARIABLE, "기숙사 식당", 5000),
-    FOOD_COURT(RestaurantType.FIXED, "푸드 코트", null),
-    SNACK_CORNER(RestaurantType.FIXED, "스낵 코너", null),
-    HAKSIK(RestaurantType.VARIABLE, "학생 식당", 5000);
+    DODAM("도담 식당", 6000),
+    DORMITORY("기숙사 식당", 5000),
+    FOOD_COURT( "푸드 코트", null),
+    SNACK_CORNER( "스낵 코너", null),
+    HAKSIK("학생 식당", 5000);
 
-    private RestaurantType restaurantType;
     private String restaurantName;
     private Integer restaurantPrice;
+
 
     @JsonCreator
     public static Restaurant from(String restaurantName) {
@@ -28,17 +28,8 @@ public enum Restaurant {
             .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_RESTAURANT));
     }
 
-    Restaurant(RestaurantType restaurantType, String description, Integer price) {
-        this.restaurantType = restaurantType;
-        this.restaurantName = description;
+    Restaurant(String restaurantName, Integer price) {
+        this.restaurantName = restaurantName;
         this.restaurantPrice = price;
-    }
-
-    public boolean isFixed() {
-        return this.restaurantType == RestaurantType.FIXED;
-    }
-
-    public boolean isVariable() {
-        return this.restaurantType == RestaurantType.VARIABLE;
     }
 }
