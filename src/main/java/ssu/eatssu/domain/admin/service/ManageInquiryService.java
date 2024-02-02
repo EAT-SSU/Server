c6703e1af365a621ff66cdb07e0a2734464cd769
SY2on <sjk8w7@gmail.com>
2024-02-02 13:22:43 +0900

    [#42] Feat: 문의 상태 변경 API 개발

--- src/main/java/ssu/eatssu/domain/admin/service/ManageInquiryService.java
+++ src/main/java/ssu/eatssu/domain/admin/service/ManageInquiryService.java
@@ -6,17 +6,29 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ssu.eatssu.domain.admin.dto.InquiryLine;
 import ssu.eatssu.domain.admin.dto.PageWrapper;
+import ssu.eatssu.domain.admin.dto.UpdateStatusRequest;
 import ssu.eatssu.domain.admin.persistence.LoadInquiryRepository;
+import ssu.eatssu.domain.admin.persistence.ManageInquiryRepository;
 import ssu.eatssu.domain.inquiry.entity.Inquiry;
+import ssu.eatssu.global.handler.response.BaseException;
+import ssu.eatssu.global.handler.response.BaseResponseStatus;
 
 @Service
 @RequiredArgsConstructor
 public class ManageInquiryService {
 
     private final LoadInquiryRepository loadInquiryRepository;
+    private final ManageInquiryRepository manageInquiryRepository;
 
     public PageWrapper<InquiryLine> getInquiryBoard(Pageable pageable) {
         Page<Inquiry> inquiries = loadInquiryRepository.findAllInquiries(pageable);
         return new PageWrapper<>(inquiries.map(InquiryLine::new));
+    }
+
+    public void updateStatus(Long inquiryId, UpdateStatusRequest request) {
+        Inquiry inquiry = manageInquiryRepository.findById(inquiryId)
+                .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_USER_INQUIRY));
+        inquiry.updateStatus(request.status());
+        manageInquiryRepository.save(inquiry);
     }
 }
