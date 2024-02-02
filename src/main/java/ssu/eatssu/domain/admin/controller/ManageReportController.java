aa1412ca80eb14364e5a26872b69873ac995e015
SY2on <sjk8w7@gmail.com>
2024-02-02 15:26:43 +0900

    [#42] Feat: 리뷰 신고 화면 조회 API 개발

---
+++ src/main/java/ssu/eatssu/domain/admin/controller/ManageReportController.java
@@ -0,0 +1,30 @@
+package ssu.eatssu.domain.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ssu.eatssu.domain.admin.dto.PageWrapper;
+import ssu.eatssu.domain.admin.dto.ReportLine;
+import ssu.eatssu.domain.admin.service.ManageReportService;
+import ssu.eatssu.global.handler.response.BaseResponse;
+
+@Controller
+@RequestMapping("/admin/reports")
+@RequiredArgsConstructor
+public class ManageReportController {
+    private final ManageReportService manageReportService;
+
+    @ResponseBody
+    @GetMapping("")
+    public BaseResponse<PageWrapper<ReportLine>> reportPage(@PageableDefault(size = 20) Pageable pageable,
+                                                            Model model) {
+        PageWrapper<ReportLine> reportLinePage = manageReportService.getReportBoard(pageable);
+        return BaseResponse.success(reportLinePage);
+    }
+
+}
