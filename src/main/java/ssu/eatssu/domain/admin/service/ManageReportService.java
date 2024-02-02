9774430003b2ad938c53ab6fef722fcac4a8c62d
SY2on <sjk8w7@gmail.com>
2024-02-02 15:40:21 +0900

    [#42] Feat: 신고 삭제 API 개발

--- src/main/java/ssu/eatssu/domain/admin/service/ManageReportService.java
+++ src/main/java/ssu/eatssu/domain/admin/service/ManageReportService.java
@@ -7,15 +7,21 @@ import org.springframework.stereotype.Service;
 import ssu.eatssu.domain.admin.dto.PageWrapper;
 import ssu.eatssu.domain.admin.dto.ReportLine;
 import ssu.eatssu.domain.admin.persistence.LoadReportRepository;
+import ssu.eatssu.domain.admin.persistence.ManageReportRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ManageReportService {
-    private final LoadReportRepository loadReportRepository;
 
+    private final LoadReportRepository loadReportRepository;
+    private final ManageReportRepository manageReportRepository;
 
     public PageWrapper<ReportLine> getReportBoard(Pageable pageable) {
         Page<ReportLine> reports = loadReportRepository.findAll(pageable);
         return new PageWrapper<>(reports);
+    }
+
+    public void delete(Long reportId) {
+        manageReportRepository.deleteById(reportId);
     }
 }
