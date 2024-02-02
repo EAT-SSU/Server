9905fa8c1c453089d912f0a686c9ac74c6cdc6d4
SY2on <sjk8w7@gmail.com>
2024-02-02 15:40:21 +0900

    [#42] Feat: 신고 삭제 API 개발

--- src/main/java/ssu/eatssu/domain/admin/controller/ManageReportController.java
+++ src/main/java/ssu/eatssu/domain/admin/controller/ManageReportController.java
@@ -5,9 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import ssu.eatssu.domain.admin.dto.PageWrapper;
 import ssu.eatssu.domain.admin.dto.ReportLine;
 import ssu.eatssu.domain.admin.service.ManageReportService;
@@ -25,6 +23,13 @@ public class ManageReportController {
                                                             Model model) {
         PageWrapper<ReportLine> reportLinePage = manageReportService.getReportBoard(pageable);
         return BaseResponse.success(reportLinePage);
+    }
+
+    @ResponseBody
+    @DeleteMapping("/{reportId}")
+    public BaseResponse<?> delete(@PathVariable Long reportId) {
+        manageReportService.delete(reportId);
+        return BaseResponse.success();
     }
 
 }
