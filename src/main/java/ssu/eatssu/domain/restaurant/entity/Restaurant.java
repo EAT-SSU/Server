4e8d40744f92e91ade908e8afa9fb53ad727f2c7
soy2on <sjk8w7@gmail.com>
2024-02-24 21:59:36 +0900

    fix: Restaurant <-> RestaurantType 순환참조 제거

--- src/main/java/ssu/eatssu/domain/restaurant/entity/Restaurant.java
+++ src/main/java/ssu/eatssu/domain/restaurant/entity/Restaurant.java
@@ -10,13 +10,12 @@ import ssu.eatssu.global.handler.response.BaseResponseStatus;
 
 @Getter
 public enum Restaurant {
-    DODAM(RestaurantType.VARIABLE, "DODAM", 6000),
-    DORMITORY(RestaurantType.VARIABLE, "DORMITORY", 5000),
-    FOOD_COURT(RestaurantType.FIXED, "FOOD_COURT", null),
-    SNACK_CORNER(RestaurantType.FIXED, "SNACK_CORNER", null),
-    HAKSIK(RestaurantType.VARIABLE, "HAKSIK", 5000);
+    DODAM( "도담 식당", 6000),
+    DORMITORY( "기숙사 식당", 5000),
+    FOOD_COURT("푸드 코트", null),
+    SNACK_CORNER( "스낵 코너", null),
+    HAKSIK( "학생 식당", 5000);
 
-    private RestaurantType restaurantType;
     private String restaurantName;
     private Integer restaurantPrice;
 
@@ -27,17 +26,8 @@ public enum Restaurant {
             .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_RESTAURANT));
     }
 
-    Restaurant(RestaurantType restaurantType, String description, Integer price) {
-        this.restaurantType = restaurantType;
+    Restaurant( String description, Integer price) {
         this.restaurantName = description;
         this.restaurantPrice = price;
-    }
-
-    public boolean isFixed() {
-        return this.restaurantType == RestaurantType.FIXED;
-    }
-
-    public boolean isVariable() {
-        return this.restaurantType == RestaurantType.VARIABLE;
     }
 }
