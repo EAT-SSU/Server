91c75bc1b6a876f9f3c1954534bbab81d162f948
chaeyeon0130 <k84292330@gmail.com>
2024-11-22 17:38:43 +0900

    feat: 내가 쓴 리뷰 리스트 조회 api v2 구현

--- src/main/java/ssu/eatssu/domain/user/presentation/UserController.java
+++ src/main/java/ssu/eatssu/domain/user/presentation/UserController.java
@@ -17,6 +17,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import ssu.eatssu.domain.auth.security.CustomUserDetails;
+import ssu.eatssu.domain.review.service.MealReviewService;
+import ssu.eatssu.domain.user.dto.MyMealReviewResponse;
 import ssu.eatssu.domain.user.dto.MyReviewDetail;
 import ssu.eatssu.domain.user.dto.MyPageResponse;
 import ssu.eatssu.domain.slice.dto.SliceResponse;
@@ -33,6 +35,7 @@ public class UserController {
 
     private final UserService userService;
     private final SliceService sliceService;
+    private final MealReviewService mealReviewService;
 
     @Operation(summary = "이메일 중복 체크", description = """
         이메일 중복 체크 API 입니다.<br><br>
@@ -96,6 +99,22 @@ public class UserController {
         SliceResponse<MyReviewDetail> myReviews = sliceService.findMyReviews(customUserDetails,
             pageable,
             lastReviewId);
+        return BaseResponse.success(myReviews);
+    }
+
+    @Operation(summary = "내가 쓴 리뷰 리스트 조회 V2", description = "내가 쓴 리뷰 리스트를 조회하는 API V2 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "내가 쓴 리뷰 리스트 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 유저", content = @Content(schema = @Schema(implementation = BaseResponse.class)))
+    })
+    @GetMapping("/v2/reviews")
+    public BaseResponse<SliceResponse<MyMealReviewResponse>> getMyReviews(
+            @Parameter(description = "마지막으로 조회된 reviewId값(첫 조회시 값 필요 없음)", in = ParameterIn.QUERY) @RequestParam(required = false) Long lastReviewId,
+            @ParameterObject @PageableDefault(size = 20, sort = "date", direction = Sort.Direction.DESC) Pageable pageable,
+            @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        SliceResponse<MyMealReviewResponse> myReviews = mealReviewService.findMyReviews(customUserDetails,
+                lastReviewId,
+                pageable);
         return BaseResponse.success(myReviews);
     }
 
