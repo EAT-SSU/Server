2d39af273554c877768beef63225629e501ad339
chaeyeon0130 <k84292330@gmail.com>
2024-11-24 01:05:01 +0900

    refactor: 특정 식당 리뷰 조회 api 리팩토링

--- src/main/java/ssu/eatssu/domain/review/dto/ReviewRatingCount.java
+++ src/main/java/ssu/eatssu/domain/review/dto/ReviewRatingCount.java
@@ -1,6 +1,10 @@
 package ssu.eatssu.domain.review.dto;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import ssu.eatssu.domain.review.entity.Review;
 
 //COMMENT: 더 좋은 매개변수 명이 있을 것 같은데 제 머리로는 이게 한계입니다... 멋진 변수명으로 바꿔주세요
 public record ReviewRatingCount(long oneStarCount, long twoStarCount, long threeStarCount, long fourStarCount,
@@ -9,7 +13,10 @@ public record ReviewRatingCount(long oneStarCount, long twoStarCount, long three
     public ReviewRatingCount {
     }
 
-    public static ReviewRatingCount from(Map<Integer, Long> ratingDistribution) {
+    public static ReviewRatingCount from(List<Review> reviews) {
+        Map<Integer, Long> ratingDistribution = reviews.stream()
+                .collect(Collectors.groupingBy(Review::getRating, LinkedHashMap::new, Collectors.counting()));
+
         return new ReviewRatingCount(
                 ratingDistribution.getOrDefault(1, 0L),
                 ratingDistribution.getOrDefault(2, 0L),
