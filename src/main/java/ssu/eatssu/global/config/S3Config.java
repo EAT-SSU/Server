9b04c3886bb65a57c77da94faf070250571c6d0e
Jiwoong <jwchoi179@soongsil.ac.kr>
2025-02-13 14:54:16 +0900

    fix: S3Config 클래스에서 사용하지 않는 패키지 제거

--- src/main/java/ssu/eatssu/global/config/S3Config.java
+++ src/main/java/ssu/eatssu/global/config/S3Config.java
@@ -3,12 +3,11 @@ package ssu.eatssu.global.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class S3Config {
@@ -26,7 +25,7 @@ public class S3Config {
     public AmazonS3Client amazonS3Client() {
         AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
-        return (AmazonS3Client)AmazonS3ClientBuilder
+        return (AmazonS3Client) AmazonS3ClientBuilder
                 .standard()
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
