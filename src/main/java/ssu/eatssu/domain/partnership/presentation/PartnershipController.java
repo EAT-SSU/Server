313069e4d8ca81e7b1c411d49b8d85746846da81
chaeyeon0130 <k84292330@gmail.com>
2025-02-16 20:48:43 +0900

    feat: 제휴 api 스웨거 작성

--- src/main/java/ssu/eatssu/domain/partnership/presentation/PartnershipController.java
+++ src/main/java/ssu/eatssu/domain/partnership/presentation/PartnershipController.java
@@ -1,5 +1,11 @@
 package ssu.eatssu.domain.partnership.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -19,20 +25,40 @@ import ssu.eatssu.global.handler.response.BaseResponse;
 @RestController
 @RequestMapping("/partnerships")
 @RequiredArgsConstructor
+@Tag(name = "Partnership", description = "제휴 API")
 public class PartnershipController {
     private final PartnershipService partnershipService;
 
+    @Operation(summary = "제휴 등록", description = "제휴를 등록하는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "제휴 등록 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 대학", content = @Content(schema =
+            @Schema(implementation = BaseResponse.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 학과", content = @Content(schema =
+            @Schema(implementation = BaseResponse.class))),
+    })
     @PostMapping
     public BaseResponse<?> createPartnership(@RequestBody CreatePartnershipRequest request) {
         partnershipService.createPartnership(request);
         return BaseResponse.success();
     }
 
+    @Operation(summary = "전체 제휴 조회", description = "전체 제휴를 조회하는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "제휴 조회 성공"),
+    })
     @GetMapping
     public BaseResponse<List<PartnershipResponse>> getAllPartnerships() {
         return BaseResponse.success(partnershipService.getAllPartnerships());
     }
 
+    @Operation(summary = "개별 제휴 조회", description = "개별 제휴를 조회하는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "제휴 조회 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 제휴", content = @Content(schema =
+            @Schema(implementation = BaseResponse.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 유저", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+    })
     @GetMapping("/{partnershipId}")
     public BaseResponse<PartnershipDetailResponse> getPartnership(
             @PathVariable Long partnershipId,
@@ -40,6 +66,13 @@ public class PartnershipController {
         return BaseResponse.success(partnershipService.getPartnership(partnershipId, userDetails));
     }
 
+    @Operation(summary = "제휴 찜 등록하기/취소하기", description = "제휴 찜 등록하기/취소하기(토글) API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "제휴 찜 등록/취소 성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 제휴", content = @Content(schema =
+            @Schema(implementation = BaseResponse.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 유저", content = @Content(schema = @Schema(implementation = BaseResponse.class))),
+    })
     @PostMapping("/{partnershipId}/like")
     public BaseResponse<?> togglePartnershipLike(
             @PathVariable Long partnershipId,
