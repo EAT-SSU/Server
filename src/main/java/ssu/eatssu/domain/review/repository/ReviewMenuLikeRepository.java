49fcfd978a29dc84d56fc46ffdff713487cf5ce8
JjungminLee <julie0964@naver.com>
2025-06-25 15:48:47 +0900

    feat: ReviewMenuRepository 원복

---
+++ src/main/java/ssu/eatssu/domain/review/repository/ReviewMenuLikeRepository.java
@@ -0,0 +1,12 @@
+package ssu.eatssu.domain.review.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ssu.eatssu.domain.review.entity.Review;
+import ssu.eatssu.domain.review.entity.ReviewMenuLike;
+
+public interface ReviewMenuLikeRepository extends JpaRepository<ReviewMenuLike, Long> {
+    List<ReviewMenuLike> findByReview(Review review);
+}
