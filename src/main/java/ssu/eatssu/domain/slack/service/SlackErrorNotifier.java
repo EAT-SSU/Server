b1226156207dc85f9a934c6bbae53dac4aefec0c
JjungminLee <julie0964@naver.com>
2025-07-20 19:05:28 +0900

    feat: slack 알림 local에서도 나가는 문제 수정

--- src/main/java/ssu/eatssu/domain/slack/service/SlackErrorNotifier.java
+++ src/main/java/ssu/eatssu/domain/slack/service/SlackErrorNotifier.java
@@ -19,12 +19,11 @@ public class SlackErrorNotifier {
     }
 
     public void notify(BaseException ex) {
-        if ("prod".equals(serverEnv)) {
-            return;
-        }
         try {
             String message = SlackMessageFormat.sendServerError(ex);
-            slackService.sendSlackMessage(message, SlackChannel.SERVER_ERROR);
+            if ("prod".equals(serverEnv)) {
+                slackService.sendSlackMessage(message, SlackChannel.SERVER_ERROR);
+            }
         } catch (Exception slackEx) {
             log.warn("슬랙 전송 실패: {}", slackEx.getMessage());
         }
