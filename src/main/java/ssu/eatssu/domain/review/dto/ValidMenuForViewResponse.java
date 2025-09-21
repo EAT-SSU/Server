0f8c21a5382db3ebef08e367a90032b1901a9992
JjungminLee <julie0964@naver.com>
2025-09-21 17:49:00 +0900

    feat: 250920 식당 dto id 수정 요청 사항 반영

--- src/main/java/ssu/eatssu/domain/review/dto/ValidMenuForViewResponse.java
+++ src/main/java/ssu/eatssu/domain/review/dto/ValidMenuForViewResponse.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
@@ -15,6 +16,17 @@ import java.util.List;
 @AllArgsConstructor
 public class ValidMenuForViewResponse {
     @Schema(description = "리뷰에 포함되는 메뉴 리스트", example = "[김치볶음밥, 고구마치즈돈까스, 김자반]")
-    private List<String> menuList;
+    private List<MenuDto> menuList;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class MenuDto {
+        @Schema(description = "메뉴 ID", example = "3143")
+        private Long menuId;
+
+        @Schema(description = "메뉴 이름", example = "우삼겹갈비탕")
+        private String name;
+    }
 
 }
