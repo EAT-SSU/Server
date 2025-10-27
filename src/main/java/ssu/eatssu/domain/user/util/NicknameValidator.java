11041ed1371f2ed6e87a44394445137d1248e78f
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-10-27 16:55:43 +0900

    test: 테스트 코드 작성

--- src/main/java/ssu/eatssu/domain/user/util/NicknameValidator.java
+++ src/main/java/ssu/eatssu/domain/user/util/NicknameValidator.java
@@ -37,7 +37,7 @@ public class NicknameValidator {
 
         // 연속된 하이폰(-)
         if (nickname.matches(".*-{2,}.*")){
-            throw new BaseException(BaseResponseStatus.CONSECUTIVE_SPACES_NICKNAME);
+            throw new BaseException(BaseResponseStatus.CONSECUTIVE_HYPHEN_NICKNAME);
         }
 
         // 한글 영어가 아닌 것으로 시작
@@ -50,8 +50,8 @@ public class NicknameValidator {
             throw new BaseException(BaseResponseStatus.INVALID_END_OF_NICKNAME);
         }
 
-        // 한글, 숫자, 영어, 하이폰(-)을 제외한 것이 포함됨.
-        if (!nickname.matches("^[A-Za-z0-9가-힣-]+$")){
+        // 한글, 숫자, 영어, 하이폰(-), 공백을 제외한 것이 포함됨.
+        if (!nickname.matches("^[A-Za-z0-9가-힣- ]+$")){
             throw new BaseException(BaseResponseStatus.INVALID_SYMBOL_NICKNAME);
         }
     }
