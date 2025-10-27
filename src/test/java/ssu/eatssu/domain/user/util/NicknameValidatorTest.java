daaa23b74fdc0fce972df4b9321d685d7e769a6f
나용준 <dydwnssk99@gmail.com>
2025-10-27 21:18:21 +0900

    feat: 욕설, 운영자 혼동, 브랜드 이름 포함 닉네임 필터링 추가 (#276)
    * feat: 단독 브랜드명 사용/ 관리자 혼동 닉네임 검증 추가
    
    * feat: 욕설 필터링 추가
    
    * fix: 오타 정정
    
    ---------
    
    Co-authored-by: 나용준 <141994188+youngJun99@users.noreply.github.com>

--- src/test/java/ssu/eatssu/domain/user/util/NicknameValidatorTest.java
+++ src/test/java/ssu/eatssu/domain/user/util/NicknameValidatorTest.java
@@ -2,7 +2,6 @@ package ssu.eatssu.domain.user.util;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import ssu.eatssu.domain.user.config.UserProperties;
 import ssu.eatssu.global.handler.response.BaseException;
 import ssu.eatssu.global.handler.response.BaseResponseStatus;
 
@@ -11,7 +10,38 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class NicknameValidatorTest {
 
-    private NicknameValidator nicknameValidator = new NicknameValidator(new UserProperties());
+    private NicknameValidator nicknameValidator = new NicknameValidator();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"ㅅㅂ","뻐킹","미친사람"})
+    void 비속어_포함_시_예외가_발생한다(String input) {
+        // when & then
+        assertThatThrownBy(()->nicknameValidator.validateNickname(input))
+                .isInstanceOf(BaseException.class)
+                .extracting("status")
+                .isEqualTo(BaseResponseStatus.PROFANITY_NICKNAME);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"잇슈","EAT-SSU","eatssu"})
+    void 단독으로_브랜드명_사용_시_예외가_발생한다(String input) {
+        // when & then
+        assertThatThrownBy(()->nicknameValidator.validateNickname(input))
+                .isInstanceOf(BaseException.class)
+                .extracting("status")
+                .isEqualTo(BaseResponseStatus.SERVICE_BRAND_NICKNAME);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"이슈 운영자","서버 관리자","admin입니다"})
+    void 관리자로_혼동될_수_있는_단어를_포함하면_예외가_발생한다(String input) {
+        // when & then
+        assertThatThrownBy(()->nicknameValidator.validateNickname(input))
+                .isInstanceOf(BaseException.class)
+                .extracting("status")
+                .isEqualTo(BaseResponseStatus.ADMIN_MANAGER_NICKNAME);
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"유효한 닉네임","available nic","가능한-닉네임","유-효-한-닉-네-임","유 효 한 닉 1 2 a"})
