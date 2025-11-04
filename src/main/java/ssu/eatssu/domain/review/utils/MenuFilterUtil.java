bc51202703dc68c5668ab4d6471e54844852d0a2
나용준 <dydwnssk99@gmail.com>
2025-11-04 20:18:32 +0900

    fix: 리뷰 필터 수정 (#286)
    * fix: 정규 표현식 및 KEYWORD 추가
    
    * fix: 닉네임 생성 규칙 공백 추가
    
    ---------
    
    Co-authored-by: 나용준 <141994188+youngJun99@users.noreply.github.com>

--- src/main/java/ssu/eatssu/domain/review/utils/MenuFilterUtil.java
+++ src/main/java/ssu/eatssu/domain/review/utils/MenuFilterUtil.java
@@ -8,11 +8,14 @@ public class MenuFilterUtil {
 
     private static final Set<String> EXCLUDED_KEYWORDS = Set.of(
             "작은밥", "쌀밥", "잡곡밥", "찰흑미밥", "귀리밥", "백미밥",
+            "강황밥","찰밥","흑미밥","기장밥","현미밥",
+            "미니밥","오곡밥","무밥","옥수수밥","검정콩밥",
+            "추가밥","한솥밥","수수밥","찰현미밥","차조밥","공기밥",
             "깍두기", "열무김치", "포기김치", "맛김치", "배추김치",
             "단무지", "오이피클", "야쿠르트", "요구르트", "우동국물"
                                                                );
 
-    private static final Pattern SAUCE_PATTERN = Pattern.compile(".*소스.*");
+    private static final Pattern SAUCE_PATTERN = Pattern.compile("소스$");
     private static final Pattern SINGLE_KIMCHI_PATTERN = Pattern.compile("(^|\\s)김치($|\\s)");
     private static final Pattern SINGLE_BAP_PATTERN = Pattern.compile("(^|\\s)밥($|\\s)");
 
