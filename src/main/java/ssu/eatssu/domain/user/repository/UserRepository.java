074c967425aafe0403bd98db994fe5e38879ef82
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-11-06 21:22:25 +0900

    feat: 마이그레이션용 임시 API 생성

--- src/main/java/ssu/eatssu/domain/user/repository/UserRepository.java
+++ src/main/java/ssu/eatssu/domain/user/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package ssu.eatssu.domain.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ssu.eatssu.domain.user.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -15,4 +18,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByProviderId(String providerId);
 
     Optional<User> findByNickname(String nickname);
+
+    @Query(value = "SELECT * FROM user WHERE nickname REGEXP '^user-.{4}$'", nativeQuery = true)
+    List<User> findByNicknameLikeUserPattern();
 }
