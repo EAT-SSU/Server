87bcdca37fc4c088238ffd99c107ebf2f4cd69dd
나용준 <141994188+youngJun99@users.noreply.github.com>
2025-12-07 17:28:30 +0900

    feat: 기능 구현

---
+++ src/main/java/ssu/eatssu/domain/auth/dto/AppleLoginRequestV2.java
@@ -0,0 +1,15 @@
+package ssu.eatssu.domain.auth.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import ssu.eatssu.domain.user.entity.DeviceType;
+import ssu.eatssu.global.log.annotation.LogMask;
+
+// 애플 계정을 통해서 갤럭시 기기에도 접속을 할 수도 있다고 생각해서, DeviceType을 받도록 설계
+@Schema(title = "애플 로그인 및 회원가입 V2")
+public record AppleLoginRequestV2(
+        @LogMask
+        @Schema(description = "identityToken", example = "eyJraWQiOiJXNldjT0tCIiwiYWxnIjoi...")
+        String identityToken,
+        @Schema(description = "deviceType", example = "iOS")
+        DeviceType deviceType
+) {}
