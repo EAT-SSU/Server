5447d0ce2b4c2d321d9a87e8598fecb7f692f0d9
나용준 <dydwnssk99@gmail.com>
2026-02-01 23:23:41 +0900

    fix: 로그인 문제 해결 (#339)
    Co-authored-by: 나용준 <141994188+youngJun99@users.noreply.github.com>

--- src/main/java/ssu/eatssu/domain/auth/security/CustomUserDetailsService.java
+++ src/main/java/ssu/eatssu/domain/auth/security/CustomUserDetailsService.java
@@ -17,9 +17,10 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    // OrderByIdAsc로 해줌으로써 처음 생성된 계정으로 연결된다.
     @Override
     public UserDetails loadUserByUsername(String username) {
-        User user = userRepository.findByEmail(username)
+        User user = userRepository.findFirstByEmailOrderByIdAsc(username)
                                   .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_USER));
         return new CustomUserDetails(user);
     }
