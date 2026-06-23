dcaddc35d9b159da5a12a27f78b51b69f54e3ad9
pooreumjung <pooreumjung02@naver.com>
2026-06-23 16:05:26 +0900

    refactor: 로그 테스트 리플렉션 제거

--- src/test/java/ssu/eatssu/global/log/ControllerLogAspectTest.java
+++ src/test/java/ssu/eatssu/global/log/ControllerLogAspectTest.java
@@ -6,7 +6,6 @@ import ssu.eatssu.domain.admin.dto.LoginRequest;
 import ssu.eatssu.domain.user.dto.Tokens;
 import ssu.eatssu.global.handler.response.BaseResponse;
 
-import java.lang.reflect.Method;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,7 +20,7 @@ class ControllerLogAspectTest {
         BaseResponse<Tokens> response = BaseResponse.success(new Tokens("access-token", "refresh-token"));
 
         // when
-        String result = invokeGetResponseLog("/oauths/kakao", response);
+        String result = controllerLogAspect.getResponseLog("/oauths/kakao", response);
 
         // then
         assertThat(result).isEqualTo("[response-body-skipped]");
@@ -34,7 +33,7 @@ class ControllerLogAspectTest {
         BaseResponse<String> response = BaseResponse.success("ok");
 
         // when
-        String result = invokeGetResponseLog("/meals", response);
+        String result = controllerLogAspect.getResponseLog("/meals", response);
 
         // then
         assertThat(result).contains("ok");
@@ -46,23 +45,11 @@ class ControllerLogAspectTest {
         LoginRequest request = new LoginRequest("admin", "password");
 
         // when
-        Map<String, Object> result = invokeToSafeMap(request);
+        Map<String, Object> result = controllerLogAspect.toSafeMap(request);
 
         // then
         assertThat(result).containsEntry("loginId", "admin");
         assertThat(result).containsEntry("password", "***");
     }
 
-    private String invokeGetResponseLog(String uri, Object result) throws Exception {
-        Method method = ControllerLogAspect.class.getDeclaredMethod("getResponseLog", String.class, Object.class);
-        method.setAccessible(true);
-        return (String) method.invoke(controllerLogAspect, uri, result);
-    }
-
-    @SuppressWarnings("unchecked")
-    private Map<String, Object> invokeToSafeMap(Object arg) throws Exception {
-        Method method = ControllerLogAspect.class.getDeclaredMethod("toSafeMap", Object.class);
-        method.setAccessible(true);
-        return (Map<String, Object>) method.invoke(controllerLogAspect, arg);
-    }
 }
