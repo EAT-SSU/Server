aacb2b792f54cf4cb79996e22e0850981e23ee4f
sjinssun <seokjinsun@naver.com>
2026-07-13 22:10:47 +0900

    [Fix]DeepL 커넥션 풀 사이즈 및 타임아웃 조정

--- src/main/java/ssu/eatssu/domain/review/infrastructure/DeepLConfig.java
+++ src/main/java/ssu/eatssu/domain/review/infrastructure/DeepLConfig.java
@@ -2,6 +2,8 @@ package ssu.eatssu.domain.review.infrastructure;
 
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -10,13 +12,26 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class DeepLConfig {
 
+    private static final int MAX_CONN_PER_ROUTE = 15;
+    private static final int MAX_CONN_TOTAL = 15;
+    private static final int CONNECT_TIMEOUT_MS = 2000;
+    private static final int CONNECTION_REQUEST_TIMEOUT_MS = 3000;
+    private static final int READ_TIMEOUT_MS = 3000;
+
     @Bean
     public RestTemplate deeplRestTemplate() {
-        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        CloseableHttpClient client = HttpClientBuilder.create().build();
-        factory.setConnectTimeout(2000);
-        factory.setReadTimeout(3000);
-        factory.setHttpClient(client);
+        PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+                                                                                                          .setMaxConnPerRoute(MAX_CONN_PER_ROUTE)
+                                                                                                          .setMaxConnTotal(MAX_CONN_TOTAL)
+                                                                                                          .build();
+        CloseableHttpClient client = HttpClientBuilder.create()
+                                                        .setConnectionManager(connectionManager)
+                                                        .build();
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(client);
+        factory.setConnectTimeout(CONNECT_TIMEOUT_MS);
+        factory.setConnectionRequestTimeout(CONNECTION_REQUEST_TIMEOUT_MS);
+        factory.setReadTimeout(READ_TIMEOUT_MS);
         return new RestTemplate(factory);
     }
 }
