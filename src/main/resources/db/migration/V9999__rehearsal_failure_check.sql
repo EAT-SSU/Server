a5287fa9ef81a5521affdd29e6fe270fcba6ea06
pooreumjung <pooreumjung02@naver.com>
2026-08-19 18:18:35 +0900

    test: 마이그레이션 리허설 검증 코드 제거

--- src/main/resources/db/migration/V9999__rehearsal_failure_check.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO review (review_id, created_date, modified_date)
-SELECT existing_review.review_id, NOW(6), NOW(6)
-FROM (
-    SELECT review_id
-    FROM review
-    LIMIT 1
-) existing_review;
