429073e5e4ea08b9d529980408dd805be9d3d517
pooreumjung <pooreumjung02@naver.com>
2026-09-01 00:09:58 +0900

    test: menu DTO 변환 테스트 추가

---
+++ src/test/java/ssu/eatssu/domain/menu/presentation/dto/response/MenuResponseDtoTest.java
@@ -0,0 +1,58 @@
+package ssu.eatssu.domain.menu.presentation.dto.response;
+
+import org.junit.jupiter.api.Test;
+import ssu.eatssu.domain.menu.entity.Menu;
+import ssu.eatssu.domain.user.entity.Language;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+class MenuResponseDtoTest {
+
+    @Test
+    void briefMenuUsesTranslationOnlyForMainMenu() {
+        Menu menu = mock(Menu.class);
+        given(menu.getId()).willReturn(1L);
+        given(menu.getName()).willReturn("돈가스");
+
+        BriefMenuResponse translated = new BriefMenuResponse(menu, Map.of("돈가스", "Pork cutlet"));
+        BriefMenuResponse original = new BriefMenuResponse(menu, Map.of());
+
+        assertThat(translated.getName()).isEqualTo("Pork cutlet");
+        assertThat(translated.isMain()).isTrue();
+        assertThat(original.getName()).isEqualTo("돈가스");
+        assertThat(original.isMain()).isFalse();
+    }
+
+    @Test
+    void menuResponsesMapLocalizedNameAndCollectCategories() {
+        Menu menu = mock(Menu.class);
+        given(menu.getId()).willReturn(1L);
+        given(menu.getNameByLanguage(Language.EN)).willReturn("Pork cutlet");
+        given(menu.getPrice()).willReturn(6000);
+        MenuResponse response = MenuResponse.from(menu, 4.5, Language.EN);
+        CategoryWithMenusResponse category = CategoryWithMenusResponse.of("한식", List.of(response));
+        MenuRestaurantResponse restaurant = MenuRestaurantResponse.init();
+        restaurant.add(category);
+        restaurant.addAll(List.of(category));
+
+        assertThat(response.getName()).isEqualTo("Pork cutlet");
+        assertThat(response.getRating()).isEqualTo(4.5);
+        assertThat(restaurant.getCategoryMenuListCollection()).containsExactly(category, category);
+    }
+
+    @Test
+    void menusInMealMapsAllBriefMenus() {
+        Menu menu = mock(Menu.class);
+        given(menu.getId()).willReturn(1L);
+        given(menu.getName()).willReturn("라면");
+
+        MenusInMealResponse response = MenusInMealResponse.from(List.of(menu), Map.of());
+
+        assertThat(response.getBriefMenus()).extracting(BriefMenuResponse::getName).containsExactly("라면");
+    }
+}
