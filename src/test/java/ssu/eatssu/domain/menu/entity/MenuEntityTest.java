aa2a845baf619388038d06b49fff3ce149b815d5
pooreumjung <pooreumjung02@naver.com>
2026-09-01 00:19:12 +0900

    test: menu entity 상태 테스트 추가

---
+++ src/test/java/ssu/eatssu/domain/menu/entity/MenuEntityTest.java
@@ -0,0 +1,51 @@
+package ssu.eatssu.domain.menu.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+import ssu.eatssu.domain.menu.entity.constants.TimePart;
+import ssu.eatssu.domain.restaurant.entity.Restaurant;
+
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MenuEntityTest {
+
+    @Test
+    void menuTracksDiscontinuedAndLikeStates() {
+        Menu menu = Menu.createFixed("돈가스", Restaurant.FOOD_COURT, 6000, null);
+
+        menu.changeDiscontinuedStatus();
+        menu.increaseLikeCount();
+        menu.changeLikeStatus(false);
+
+        assertThat(menu.isContinued()).isFalse();
+        assertThat(menu.getLikeCount()).isZero();
+        menu.cancelLike(true);
+        assertThat(menu.getLikeCount()).isEqualTo(-1);
+    }
+
+    @Test
+    void nullLikeCountIsInitializedBeforeUpdates() {
+        Menu menu = Menu.createVariable("라면", Restaurant.DODAM);
+        ReflectionTestUtils.setField(menu, "likeCount", null);
+        ReflectionTestUtils.setField(menu, "unlikeCount", null);
+
+        menu.increaseLikeCount();
+        menu.decreaseLikeCount();
+
+        assertThat(menu.getLikeCount()).isEqualTo(0);
+    }
+
+    @Test
+    void mealReturnsMenusAndNamesInInsertionOrder() {
+        Menu first = Menu.createVariable("라면", Restaurant.DODAM);
+        Menu second = Menu.createVariable("김치", Restaurant.DODAM);
+        Meal meal = new Meal(new Date(), TimePart.LUNCH, Restaurant.DODAM);
+        meal.addMealMenu(MealMenu.builder().meal(meal).menu(first).build());
+        meal.addMealMenu(MealMenu.builder().meal(meal).menu(second).build());
+
+        assertThat(meal.getMenus()).containsExactly(first, second);
+        assertThat(meal.getMenuNames()).containsExactly("라면", "김치");
+    }
+}
